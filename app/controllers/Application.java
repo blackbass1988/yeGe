128d9db6579060bb104650681ec3d1010f789b27
Oleg Salionov <o.salionov@zmeke.com>
2012-12-24 18:05:01 +1100

    some more stubs

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,21 +10,27 @@ import java.util.Random;
 public class Application extends Controller {
 
 
-    static String productName = "EggPunch";
+    static String[] productName = {"EggPunch", "FunnyLandia", "Texas Poker"};
+
     static String[] positive = {"Охуенчик", "Вы лучшие!"};
     static String[] positive2 = {"Хардкора", "cисек", "писек", "холодца", "радости зарплату программистам",
             "разрешение на безвизовый въезд", "больше новых уровней" };
 
+    static String[] howIt = {"прекрасна", "лучше всех", "разрушает мой мозг", "заставила меня бросить жену и работу" };
 
-    static String[] author = {"Артур", "Timur", "La ji zao", "huida"};
-    static String[] phrases = {"${positive}, ваша игра ${name}, даешь больше ${positive2}",
-            "${name} - ${positive}", };
+
+    static String[] author = {"Артур", "Timur", "La ji zao", "Felix", "Oleg",  "Sergey", "Alex", "Sandr"};
+    static String[] phrases = {"${positive}, ваша игра ${name} ${howIt}, даешь ${positive2}",
+            "${name} - ${positive}",
+            "нуежели ${name} раньше не могли выпустить? Даешь ${positive2}, ${positive2}, ${positive2}, ${positive2}",
+
+    };
     static String[] headers = {"Вау", "Заебись", "Никогда лучше не слыхал!"};
     static Random rand = new Random();
 
     public static void index() {
 
-        int countOfReviews = 100;
+        int countOfReviews = 10;
         List<Review> reviews = new LinkedList<>();
 
         for (int i = 0; i<countOfReviews; i++){
@@ -39,13 +45,19 @@ public class Application extends Controller {
         String author = getAuthor();
         String positive = getPositive();
         String positive2 = getPositive2();
+        String howIt = getHowIt();
 
         Review review = new Review(author, header, getPhrase());
-        review.replace("name", productName);
+        review.replace("name", getProductName());
         review.replace("positive", positive);
         review.replace("positive2", positive2);
-
+        review.replace("howIt", howIt);
+        review.cleanPunctuation();
         return review;
+    }
+
+    private static String getProductName() {
+        return getRandomFromArray(productName);
     }
 
     private static String getPhrase() {
@@ -69,5 +81,9 @@ public class Application extends Controller {
     }
     public static String getPositive2() {
         return getRandomFromArray(positive2);
+    }
+
+    public static String getHowIt() {
+        return getRandomFromArray(howIt);
     }
 }
