e06d2c3a9901f4997cd44b286b40189a3ea5332d
Oleg Salionov <o.salionov@zmeke.com>
2012-12-25 20:25:06 +1100

    minor in scaffold

--- app/controllers/Administration.java
+++ app/controllers/Administration.java
@@ -9,23 +9,4 @@ public class Administration extends Controller {
     public static void index() {
         render();
     }
-
-    public static void newInstance(String type) {
-        switch (type) {
-            case "author":
-                redirect("/authors/list");
-            case "header":
-                redirect("/headers/list");
-            case "phrase":
-                redirect("/phrases/list");
-            case "placeholder":
-                redirect("/placeholders/list");
-            case "project":
-                redirect("/projects/list");
-            case "template":
-                redirect("/reviewtemplates/list");
-            default: renderText("Неизвестный тип %s", type);
-        }
-
-    }
 }
